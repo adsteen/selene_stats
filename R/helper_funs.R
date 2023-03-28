4c79f98ed927bbce241502a53c67553d7566142c
Drew Steen <andrew.decker.steen@gmail.com>
2023-03-28 17:51:07 -0400

    y'know, stuff

--- R/helper_funs.R
+++ R/helper_funs.R
@@ -1,4 +1,21 @@
-read_gene_freq_data <- function(filename) {
+# Read in raw data, removes "total" row
+# Makes the df long
+
+read_gene_freq_data <- function(path, rename.vec = NULL) {
+  d <- read_csv(path=path, sheet=sheet, range=range) 
+  
+  if(!is.null(rename.vec)) {
+    d <- rename(d, rename.vec)
+  }
+  browser()
+    mutate(n.genes = as.numeric(n.genes)) %>%
+    pivot_longer(cols = -1, names_to = "category", values_to = "count") %>%
+    group_by(category) %>%
+    mutate(freq = count / sum(count, na.rm = TRUE)) %>%
+    arrange(category, count)
+}
+
+read_gene_freq_data.OLD <- function(filename) {
   d <- read_csv(filename)
   
   # Remove the "total" row if there is one
